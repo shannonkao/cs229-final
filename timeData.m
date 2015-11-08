d5f9b4fb350279ffdb7779eb315d9460395c3e10
mhneub <marianna.h.neubauer@gmail.com>
2015-11-08 01:04:12 -0800

    added timeData.m
    This file gets the transformation matrices at the correct time points
    (30 fps) for every joint in every bvh file in a subfolder (that I
    hardcoded as ‘CMU_walkcycles’). This can be made into a function later.

---
+++ timeData.m
@@ -0,0 +1,46 @@
+
+oldfolder = cd('CMU_walkcycles');
+
+files = dir('*.bvh');
+cd(oldfolder);
+num_points = 10000;
+num_files = size(files,1);
+
+transformations = struct([]);
+time_step = zeros(1,num_files);
+i = 1;
+for i = 1:num_files
+    [skeleton,time] = loadbvh(files(i).name);
+    
+    time_step(i) = round(0.033/(time(2)-time(1)));
+    if size(time,2)/time_step(i) < num_points
+        num_points = size(time,2)/time_step(i);
+    end
+    
+    joint_info = struct([]);
+    for j = 1:size(skeleton,2)
+        joint_info(j).name = skeleton(j).name;
+        joint_info(j).nestdepth = skeleton(j).nestdepth;
+        joint_info(j).parent = skeleton(j).parent;
+        joint_info(j).t_xyz = skeleton(j).t_xyz(:,1:time_step(i):end);
+        joint_info(j).R_xyz = skeleton(j).R_xyz(:,1:time_step(i):end);
+        joint_info(j).T = skeleton(j).T(:,:,1:time_step(i):end);
+    end
+    transformations = [transformations; joint_info];
+end
+
+num_points = floor(num_points);
+
+for i = 1:size(transformations,1)
+    for j = 1:size(transformations,2)
+        if size(transformations(i,j).t_xyz,1) > 0
+            transformations(i,j).t_xyz = transformations(i,j).t_xyz(:,1:num_points);
+        end
+        if size(transformations(i,j).R_xyz,1) > 0
+            transformations(i,j).R_xyz = transformations(i,j).R_xyz(:,1:num_points);
+        end
+        if size(transformations(i,j).T,1) > 0
+            transformations(i,j).T = transformations(i,j).T(:,:,1:num_points);
+        end
+    end
+end
