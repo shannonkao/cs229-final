d7e3985aa43417c3c760a508af7d1d51c17c3309
mhneub <marianna.h.neubauer@gmail.com>
2015-11-12 17:36:44 -0800

    changed timeData to getTimeData

--- getTimeData.m
+++ getTimeData.m
@@ -1,16 +1,17 @@
-
-oldfolder = cd('CMU_walkcycles');
+function [transformations] = getTimeData(folderName)
+oldfolder = cd(folderName);
 
 files = dir('*.bvh');
 cd(oldfolder);
+
 num_points = 10000;
 num_files = size(files,1);
 
 transformations = struct([]);
 time_step = zeros(1,num_files);
-i = 1;
+
 for i = 1:num_files
-    [skeleton,time] = loadbvh(files(i).name);
+    [skeleton,time] = loadbvh(fullfile(folderName, files(i).name));
     
     time_step(i) = round(0.033/(time(2)-time(1)));
     if size(time,2)/time_step(i) < num_points
@@ -29,8 +30,8 @@ for i = 1:num_files
     transformations = [transformations; joint_info];
 end
 
+% truncate data
 num_points = floor(num_points);
-
 for i = 1:size(transformations,1)
     for j = 1:size(transformations,2)
         if size(transformations(i,j).t_xyz,1) > 0
@@ -43,4 +44,6 @@ for i = 1:size(transformations,1)
             transformations(i,j).T = transformations(i,j).T(:,:,1:num_points);
         end
     end
+end
+
 end
---
+++ timeDataTest.m
@@ -0,0 +1,5 @@
+% timePoints is a mxn struct where m is the number of samples (walkcycles)
+% and n is the number of joints. Does not yet work with varibale number of
+% joints
+
+timePoints = getTimeData('CMU_walkcycles');
